52e0fbae1a1101562bc961a806dabe3a24177517
Gil Gonçalves <lursty@gmail.com>
2016-12-05 16:24:15 +0000

    Changed how background appears (now is centered)

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -4,7 +4,7 @@
 unclutter -idle 2 &
 
 # Setup background
-feh --bg-scale ~/.xdg/background.jpg &
+feh --bg-center ~/.xdg/background.jpg &
 
 # Setup Caps Lock to Ctrl
 setxkbmap gb && sh ~/.xmodmap &
