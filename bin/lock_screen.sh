e4dae0e40e53f5dbf5336e61595764438ee3af96
Gil Gonçalves <lursty@gmail.com>
2017-01-05 13:41:41 +0000

    Many goodies
    - Made python editing faster on vim
    - Caching for locking pc
    - added show-tags git alias
    - added dps docker alias
    - addded `-v` flag to commit alias in git
    - and some more..

--- bin/lock_screen.sh
+++ bin/lock_screen.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+if [ -a /tmp/screenshotblur.png ]; then
+    i3lock -i /tmp/screenshotblur.png
+else
+    scrot /tmp/screen_locked.png
+    convert /tmp/screen_locked.png -blur 0x5 /tmp/screenshotblur.png
+    i3lock -i /tmp/screenshotblur.png
+fi
