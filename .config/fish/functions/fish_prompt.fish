7841f9e0a5709c4f168d20c25348ec6e72a38e6a
Gil Gonçalves <lursty@gmail.com>
2019-02-20 07:29:51 +0000

    Move git status to the right prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,11 +1,8 @@
-# Defined in /tmp/fish.NylptW/fish_prompt.fish @ line 2
+# Defined in /tmp/fish.NqscGF/fish_prompt.fish @ line 2
 function fish_prompt
 	set last_status $status
 
     set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
-
-    printf '%s ' (__fish_git_prompt)
+    printf '%s ' (prompt_pwd)
     set_color normal
 end
--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,4 +1,6 @@
-# Defined in /tmp/fish.5Avej8/fish_right_prompt.fish @ line 2
+# Defined in /tmp/fish.KwsijA/fish_right_prompt.fish @ line 2
 function fish_right_prompt --description 'Write out the right prompt'
-	echo -n -s " [" (date '+%H:%M:%S') "]"
+	printf '%s ' (__fish_git_prompt)
+    printf '[%s]' (date '+%H:%M:%S')
+    set_color normal
 end
