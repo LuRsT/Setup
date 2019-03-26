9c5e9c7a5cc5d5de41acfac5c4ecc9fa505b49a0
Gil Gonçalves <lursty@gmail.com>
2019-03-26 07:49:05 +0000

    Right prompt config for virtualenv

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,12 @@
 # Defined in /tmp/fish.WsPgJN/fish_right_prompt.fish @ line 2
 function fish_right_prompt --description 'Write out the right prompt'
-	printf '%s' (__fish_git_prompt)
     #printf '[%s]' (date '+%H:%M:%S')
+
+    if set -q VIRTUAL_ENV
+        echo -n -s "(" (basename "$VIRTUAL_ENV") ")"
+    end
+
+    printf '%s' (__fish_git_prompt)
+
     set_color normal
 end
