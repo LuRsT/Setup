8390f189d0b9629ae3c137a55e7f29cfa50185c9
Gil Gonçalves <lursty@gmail.com>
2019-07-26 09:48:48 +0100

    Update autostart script

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -3,14 +3,10 @@
 # Hide mouse cursor while typing
 unclutter -idle 2 &
 
-redshift -l 51.5073:0.12755 &
+redshift -P -O 5000K &
 
 # Setup background
 setup_background &
 
 # Setup Caps Lock to Ctrl
 setxkbmap gb && sh ~/.xmodmap &
-
-start-pulseaudio-X11 &
-
-dropbox start &
