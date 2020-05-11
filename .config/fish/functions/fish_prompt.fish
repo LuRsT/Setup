4b9b251c12f9cfe27a4e227cd90149662729a71c
Gil Gonçalves <lursty@gmail.com>
2020-05-11 07:52:39 +0100

    Remove right side of prompt
    Made this change to make it easier to use the terminal on emacs, but I
    think this will also help with copy pasting.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,18 @@
 # Defined in /tmp/fish.NqscGF/fish_prompt.fish @ line 2
 function fish_prompt
-	set last_status $status
+    set last_status $status
 
     set_color $fish_color_cwd
+
+    if set -q VIRTUAL_ENV
+        echo -n -s "(" (basename "$VIRTUAL_ENV") ")"
+    end
+
+    printf '%s ' (__fish_git_prompt)
+
+    set_color normal
+
     printf '%s ' (prompt_pwd)
+
     set_color normal
 end
