f1361f1f83a415deaa8475d109b0452e45308bf1
Gil Gonçalves <lursty@gmail.com>
2020-06-08 19:38:33 +0100

    More changes to emacs config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -180,3 +180,24 @@
 
 (map! :map evil-window-map
       "SPC" #'rotate-layout)
+
+(after! company
+  (setq company-idle-delay 0.5
+        company-minimum-prefix-length 2)
+  (setq company-show-numbers t)
+(add-hook 'evil-normal-state-entry-hook #'company-abort)) ;; make aborting less annoying.
+
+(setq-default history-length 1000)
+(setq-default prescient-history-length 1000)
+
+(set-company-backend! '(text-mode
+                        markdown-mode
+                        gfm-mode)
+  '(:seperate company-ispell
+              company-files
+              company-yasnippet))
+
+(after! flyspell (require 'flyspell-lazy) (flyspell-lazy-mode 1))
+
+(package! org-pretty-table-mode
+ :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "88380f865a...")
