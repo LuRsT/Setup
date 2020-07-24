1da5ad2aec6e31d193c14d238d5600357d27977c
Gil Gonçalves <lursty@gmail.com>
2020-07-24 19:16:37 +0100

    Remove virtualenv from prompt
    I was having it in double since activate already does it.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -4,10 +4,6 @@ function fish_prompt
 
     set_color $fish_color_cwd
 
-    if set -q VIRTUAL_ENV
-        echo -n -s "(" (basename "$VIRTUAL_ENV") ")"
-    end
-
     printf '%s ' (__fish_git_prompt)
 
     set_color normal
