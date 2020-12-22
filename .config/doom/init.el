1c78af1b356393157935777b31041a822c7d15a4
Gil Gonçalves <lursty@gmail.com>
2020-12-22 17:59:51 +0000

    Small updates to doom config
    - Remove trailing white space on save
    - Press Enter to eval elisp
    - Enable terraform module

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -98,7 +98,7 @@
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
-       ;;terraform         ; infrastructure as code
+       terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
