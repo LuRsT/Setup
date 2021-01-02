8748e072202944dff1b26048261fae1dfc3bd007
Gil Gonçalves <lursty@gmail.com>
2021-01-02 16:33:20 +0000

    Clean up tmux right
    Since everything I need is on i3 status bar

--- .config/tmux/status-right.sh
+++ .config/tmux/status-right.sh
@@ -1,16 +1,2 @@
 #!/usr/bin/env bash
 
-DOCKER_CONTAINERS_RUNNING=$(expr "$(docker ps | wc -l)" - 1)
-DOCKER_ICON="⊞ "
-
-MEM_FREE=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2/1000000}')
-
-POMODORO_RUNNING=$(ps aux | grep pomodoro | wc -l)
-if [ $POMODORO_RUNNING -eq 2 ]
-then
-    POMO="🕛"
-else
-    POMO=""
-fi
-
-echo -e "$POMO RAM: $MEM_FREE GB $DOCKER_ICON: $DOCKER_CONTAINERS_RUNNING"
