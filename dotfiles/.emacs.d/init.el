e2651517f4b7ce0bbfa07f034523ade5b06cc904
Gil Gonçalves <lursty@gmail.com>
2024-04-13 11:11:36 +0100

    Big update to emacs config
    - Made keybindings work with space
    - Auto-install packages
    - Transferred all configs possible from my previous doom config
    - More...

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -5,10 +5,28 @@
 (setq nano-font-family-proportional "JetBrains Mono")
 (setq nano-font-size 14)
 
+
+(setq org-directory "~/vimwiki")
+(setq org-ellipsis " ▼ ")
+(setq org-default-notes-file "~/vimwiki/Tasks.org")
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "firefox")
+
+(setq-default
+ tab-width 4                                      ; Set width for tabs
+ uniquify-buffer-name-style 'forward              ; Uniquify buffer names
+ window-combination-resize t                      ; take new window space from all other windows (not just current)
+ x-stretch-cursor t)                              ; Stretch cursor to the glyph width
+
+(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
+      evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
+      inhibit-compacting-font-caches t            ; When there are lots of glyphs, keep them in memory
+      )
+
+;; Enable all JetBrains Mono ligatures in programming modes
 (use-package ligature
 :load-path "path-to-ligature-repo"
 :config
-;; Enable all JetBrains Mono ligatures in programming modes
 (ligature-set-ligatures 'prog-mode '("-|" "-~" "---" "-<<" "-<" "--" "->" "->>" "-->" "///" "/=" "/=="
 				    "/>" "//" "/*" "*>" "***" "*/" "<-" "<<-" "<=>" "<=" "<|" "<||"
 				    "<|||" "<|>" "<:" "<>" "<-<" "<<<" "<==" "<<=" "<=<" "<==>" "<-|"
@@ -24,11 +42,13 @@
 ;; per mode with `ligature-mode'.
 (global-ligature-mode t))
 
+(use-package counsel :ensure t)
+
+;; Load paths
 (add-to-list 'load-path "~/.emacs.d/nano")
 (add-to-list 'load-path "~/.emacs.d/custom") ; My own downloaded plugins
 
 (require 'nano-layout)
-
 (require 'nano-faces)
 (require 'nano-theme)
 (require 'nano-theme-dark)
@@ -36,17 +56,16 @@
 (nano-theme-set-dark)
 (call-interactively 'nano-refresh-theme)
 
-(require 'nano-session)
-
 (require 'nano-modeline)
-
 (require 'nano-help)
 
 (provide 'nano)
 
+; Increase mini-frame size
+(setq mini-frame-default-height 12)
+
 (setq auto-save-file-name-transforms `((".", "~/.emacs-saves" t)))
 
-(setq ivy-height 8)   ;; number of lines in e.g. file-selector
 (setq mini-frame-default-height 12)
 
 ;; Which key (file in 'custom' folder)
@@ -63,21 +82,22 @@
 ;; Initialize package sources
 (require 'package)
 
+;; TODO: CHeck whether I need all these bellow
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                     ("org" . "https://orgmode.org/elpa/")
                     ("elpa" . "https://elpa.gnu.org/packages/")))
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;(unless package-archive-contents
+;  (package-refresh-contents))
 
-(use-package command-log-mode)
+(use-package command-log-mode :ensure t)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Ivy for completion and fuzzy
 (use-package ivy
+  :ensure t
   :diminish
   :bind (("C-s" . swiper)
 	 :map ivy-minibuffer-map
@@ -95,8 +115,21 @@
   :config
   (ivy-mode 1))
 
+;; number of lines in e.g. file-selector
+(setq ivy-height 8)
+
+; This config shows options by pressing `C-o` inside the search buffer.
+(setq ivy-read-action-function #'ivy-hydra-read-action)
+
 ;; Had to move this here after ivy since it requires ivy
 (require 'nano-counsel)
+
+
+;; Remove toolbar and menubar and scroll-bar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
 
 ; Make Ivy Fuzzy
 (setq ivy-re-builders-alist
@@ -104,6 +137,7 @@
 
 ;; Counsel
 (use-package counsel
+  :ensure t
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
 	 ("C-x C-f" . counsel-find-file)
@@ -119,6 +153,7 @@
 (setq evil-search-module 'evil-search)
 
 (use-package evil
+  :ensure t
   :init
   (setq evil-want-keybinding nil)
   (setq evil-want-integration t)
@@ -136,20 +171,102 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-    (package-install 'evil))
 
 (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
 (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
 (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
+;; General
+(use-package general
+  :ensure t)
+(require 'general)
+
+;; * Global Keybindings
+;; `general-define-key' acts like `evil-define-key' when :states is specified
+(general-define-key
+ :states 'motion
+ ;; swap ; and :
+ ";" 'evil-ex
+ ":" 'evil-repeat-find-char)
+;; same as
+(general-define-key
+ :states 'motion
+ ";" 'evil-ex
+ ":" 'evil-repeat-find-char)
+;; `general-def' can be used instead for `evil-global-set-key'-like syntax
+(general-def 'motion
+  ";" 'evil-ex
+  ":" 'evil-repeat-find-char)
+
+;; alternative using `general-translate-key'
+;; swap ; and : in `evil-motion-state-map'
+(general-swap-key nil 'motion
+  ";" ":")
+
+;; * Mode Keybindings
+(general-define-key
+ :states 'normal
+ :keymaps 'emacs-lisp-mode-map
+ ;; or xref equivalent
+ "K" 'elisp-slime-nav-describe-elisp-thing-at-point)
+;; `general-def' can be used instead for `evil-define-key'-like syntax
+(general-def 'normal emacs-lisp-mode-map
+  "K" 'elisp-slime-nav-describe-elisp-thing-at-point)
+
+;; * Prefix Keybindings
+;; :prefix can be used to prevent redundant specification of prefix keys
+;; again, variables are not necessary and likely not useful if you are only
+;; using a definer created with `general-create-definer' for the prefixes
+;; (defconst my-leader "SPC")
+;; (defconst my-local-leader "SPC m")
+
+(general-create-definer my-leader-def
+  ;; :prefix my-leader
+  :prefix "SPC")
+
+(general-create-definer my-local-leader-def
+  ;; :prefix my-local-leader
+  :prefix "SPC m")
+
+;; ** Global Keybindings
+(my-leader-def
+  :keymaps 'normal
+  ;; bind "SPC a"
+  "oa" 'org-agenda
+  "oc" 'org-capture
+  "g" 'magit-status
+  "b" 'ibuffer
+  ;"p Menu
+  "pp" 'projectile-switch-project
+  "pf" 'projectile-find-file
+  "pg" 'grep-find
+)
+
+;; * Settings
+;; change evil's search module after evil has been loaded (`setq' will not work)
+(general-setq evil-search-module 'evil-search)
+
 ;; Evil-org-mode
+(use-package evil-org :ensure t)
 (add-to-list 'load-path "~/.emacs.d/plugins/evil-org-mode")
 (require 'evil-org)
 (add-hook 'org-mode-hook 'evil-org-mode)
 (evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+
+;; Make TAB work for toggle only
+(defun my/org-cycle-only-current-subtree ()
+  "Cycle the visibility of the current subtree only."
+  (interactive)
+  (org-narrow-to-subtree)
+  (org-cycle)
+  (widen))
+
+(with-eval-after-load 'org
+  (define-key org-mode-map (kbd "TAB") 'my/org-cycle-only-current-subtree))
+
+
+
 (require 'evil-org-agenda)
 (evil-org-agenda-set-keys)
 
@@ -157,11 +274,6 @@
   :after evil
   :config
   (evil-collection-init))
-
-;; Remove toolbar and menubar and scroll-bar
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
 
 ;; Stop saving clipboard
 (setq x-select-enable-clipboard-manager nil)
@@ -241,7 +353,7 @@
 
 
 ;; YAML
-(use-package yaml-mode :ensure t)
+(use-package yaml :ensure t)
 
 ;; Markdown
 (use-package markdown-mode
@@ -257,6 +369,20 @@
   :config
   (editorconfig-mode 1))
 
+;; Find Dired
+(require 'find-dired)
+(setq find-ls-option '("-exec ls -ldh {} +" . "-ldh"))
+
+;; Company
+(use-package company
+  :ensure t)
+;; TODO: Check the backends I could find useful
+
+;; Flycheck
+(use-package flycheck
+  :ensure t)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
 ;; Your init file should contain only one such instance.
 ;; If there is more than one, they won't work right.
 (custom-set-variables
@@ -265,7 +391,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(neotree doom-modeline which-key counsel-projectile projectile magit evil-collection command-log-mode)))
+   '(yaml flycheck company company-mode terraform-mode ligature neotree which-key counsel-projectile projectile magit evil-collection command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
