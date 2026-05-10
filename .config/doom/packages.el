9ae26cfe191a19d9c435a84ba739a9d42bd5a47c
Gil Gonçalves <lursty@gmail.com>
2026-05-10 17:26:36 +0100

    emacs: remove doom config
    Doom is no longer used; .config/emacs/ is the active config. Delete
    the three Doom files and drop the now-dead .gitignore rules
    (!.config/doom whitelist + the two doom-specific ignore lines for
    config.el and custom.el).

--- .config/doom/packages.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
