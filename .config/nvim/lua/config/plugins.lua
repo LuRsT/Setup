7507e35d1d59378d0fc07064af9d41c5f81c6d3a
Gil Gonçalves <lursty@gmail.com>
2026-05-14 07:06:05 +0100

    Add some more shortcuts to nvim

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -89,12 +89,20 @@ require("lazy").setup({
                 },
             })
             local builtin = require("telescope.builtin")
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+            -- File shortcuts
+            vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find files' })
+            vim.keymap.set('n', '<leader>ff',       builtin.find_files, { desc = 'Find files' })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
             vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'LSP references' })
             vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Document symbols' })
+
+            -- Git shortcuts
             vim.keymap.set('n', '<leader>gl', builtin.git_commits, { desc = 'Git log' })
+            vim.keymap.set('n', '<leader>gb', builtin.git_commits, { desc = 'Git blame' })
+            vim.keymap.set('n', '<leader>gs', builtin.git_commits, { desc = 'Git status' })
+
+            -- Grep shortcuts
             vim.keymap.set('n', '<leader>rg', builtin.grep_string, { desc = 'Grep for string' })
         end,
     },
